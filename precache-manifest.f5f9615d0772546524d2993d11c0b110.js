b9e77d21fc74f36bddd10571be7ad1b6625e279e
epedalino <epedalino@gmail.com>
2020-04-10 17:39:21 -0400

    Updates

---
+++ precache-manifest.f5f9615d0772546524d2993d11c0b110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128110ee18664d1b32a1510c6b1aff67",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "580853e91f29ab43610f",
+    "url": "/drum-kit/static/css/main.c782490b.chunk.css"
+  },
+  {
+    "revision": "1098ef402046ab05717c",
+    "url": "/drum-kit/static/js/2.5e19a699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-kit/static/js/2.5e19a699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580853e91f29ab43610f",
+    "url": "/drum-kit/static/js/main.2ba99ec1.chunk.js"
+  },
+  {
+    "revision": "466071b180c2e6d022c6",
+    "url": "/drum-kit/static/js/runtime-main.f888b98e.js"
+  }
+]);
